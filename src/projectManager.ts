29eb30bbacb3d9f42de9739aaff6c5e394959d81
Mohammad Shahin <59062739+MohammadShahin@users.noreply.github.com>
2023-01-25 00:44:14 +0300

    chore: add helper methods for project manager

--- src/projectManager.ts
+++ src/projectManager.ts
@@ -1,18 +1,68 @@
 import {
   ProjectsManager,
-} from "@mohammadshahin/zero-wallet-dashboard-sdk/build/main";
+  GasTankRawType,
+  NewGasTankParams,
+} from "@mohammadshahin/zero-wallet-dashboard-sdk";
 
-const projectManager = new ProjectsManager('./config.yaml');
+const projectManager = new ProjectsManager("./config.yaml");
 
-const getReadyGasTank = async (projectApiKey: string, chainId: number) => {
-  const project = await projectManager.getProject(projectApiKey);
+async function getReadyGasTankId(
+  projectId: string,
+  chainId: string,
+  loadRelayer = true,
+) {
+  const project = await projectManager.getProjectById(projectId);
+  const chainIdNumber = parseInt(chainId, 10);
   const gasTank = await project.loadAndGetGasTankByChainId(
-    chainId,
+    chainIdNumber,
+    loadRelayer,
   );
   await gasTank.readyPromise;
 
   return gasTank;
-};
+}
+
+async function getReadyGasTankApiKey(
+  projectId: string,
+  chainId: string,
+  loadRelayer = true,
+) {
+  const project = await projectManager.getProjectByApiKey(projectId);
+  const chainIdNumber = parseInt(chainId, 10);
+  const gasTank = await project.loadAndGetGasTankByChainId(
+    chainIdNumber,
+    loadRelayer,
+  );
+  await gasTank.readyPromise;
+
+  return gasTank;
+}
+
+async function getProjectsByOwner(owner: string): Promise<unknown> {
+  const projects = await projectManager.getAllProjectsOwnerRaw(owner);
+  return projects;
+}
+
+async function getGasTanksRaw(projectId: string): Promise<GasTankRawType[]> {
+  const project = await projectManager.getProjectById(projectId);
+  const gasTanks = await project.getGasTanksRaw();
+  return gasTanks;
+}
+
+async function addGasTank(
+  projectId: string,
+  gasTankProps: NewGasTankParams,
+  whiteList: string[],
+) {
+  const project = await projectManager.getProjectById(projectId);
+  await project.addGasTank(gasTankProps, whiteList);
+}
 
 export default projectManager;
-export { getReadyGasTank };
+export {
+  getReadyGasTankId,
+  getReadyGasTankApiKey,
+  getProjectsByOwner,
+  getGasTanksRaw,
+  addGasTank,
+};
