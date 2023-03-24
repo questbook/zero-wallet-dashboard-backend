d376bfd76653587866151e833507c5a162e68ff5
Mohammad Shahin <59062739+MohammadShahin@users.noreply.github.com>
2023-03-24 14:01:35 +0300

    feat: dockerize the app

--- build.ts
+++ build.ts
@@ -14,9 +14,6 @@ import childProcess from 'child_process';
   try {
     // Remove current build
     await remove('./dist/');
-    // Copy front-end files
-    await copy('./src/public', './dist/public');
-    await copy('./src/views', './dist/views');
     // Copy back-end files
     await exec('tsc --build tsconfig.prod.json', './');
   } catch (err) {
@@ -30,7 +27,7 @@ import childProcess from 'child_process';
 function remove(loc: string): Promise<void> {
   return new Promise((res, rej) => {
     return fs.remove(loc, (err) => {
-      return (!!err ? rej(err) : res());
+      return (err ? rej(err) : res());
     });
   });
 }
@@ -41,7 +38,7 @@ function remove(loc: string): Promise<void> {
 function copy(src: string, dest: string): Promise<void> {
   return new Promise((res, rej) => {
     return fs.copy(src, dest, (err) => {
-      return (!!err ? rej(err) : res());
+      return (err ? rej(err) : res());
     });
   });
 }
@@ -52,13 +49,13 @@ function copy(src: string, dest: string): Promise<void> {
 function exec(cmd: string, loc: string): Promise<void> {
   return new Promise((res, rej) => {
     return childProcess.exec(cmd, {cwd: loc}, (err, stdout, stderr) => {
-      if (!!stdout) {
+      if (stdout) {
         logger.info(stdout);
       }
-      if (!!stderr) {
+      if (stderr) {
         logger.warn(stderr);
       }
-      return (!!err ? rej(err) : res());
+      return (err ? rej(err) : res());
     });
   });
 }
